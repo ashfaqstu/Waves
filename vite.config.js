038487cc34c7b29d3fe2e8a644fdc2a375d1a18c
Mohammad Ashfaqul Hasan <166525758+ashfaqOPU@users.noreply.github.com>
2025-06-15 19:09:06 +0600

    a

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Add this line
   plugins: [react(), tailwindcss()],
   css: {
-    modules:{
-      localsConvention:"camelCase",
+    modules: {
+      localsConvention: "camelCase",
     },
   }
 })
